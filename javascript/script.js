ff4cfb17f6a10f5c461ca13bf28a73ba8f57a747
Miguel Sanglard <miguelsanglard@gmail.com>
2023-12-01 01:11:10 -0300

    menu ativo e parametro URL

---
+++ javascript/script.js
@@ -0,0 +1,22 @@
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarproduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarproduto);
